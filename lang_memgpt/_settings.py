1de74534d7ca407d9794ee6aa5bbc79aa1022770
Zaid <siddiquizaid683@gmail.com>
2024-07-16 20:33:53 +0545

    Update _settings.py

--- lang_memgpt/_settings.py
+++ lang_memgpt/_settings.py
@@ -2,10 +2,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    pinecone_api_key: str = ""
-    pinecone_index_name: str = ""
-    pinecone_namespace: str = "ns1"
-    model: str = "claude-3-5-sonnet-20240620"
+    pinecone_api_key: str = "fd9453a9-749a-4400-9673-053edbbe70a7"
+    pinecone_index_name: str = "membot"
+    pinecone_namespace: str = "Vought"
+    model: str = "gpt-3.5-turbo"
 
 
 SETTINGS = Settings()
